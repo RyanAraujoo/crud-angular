9eb050d2c9064500cf17824404be442eed714a1b
Ryan Pablo Silva Araujo <zryanaraujoo@gmail.com>
2023-01-28 14:12:47 -0300

    feat: registration of vehicle

--- src/app/view/home/registration/registration.component.ts
+++ src/app/view/home/registration/registration.component.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
   styleUrls: ['./registration.component.scss']
 })
-export class RegistrationComponent implements OnInit {
+export class RegistrationComponent {
 
-  constructor() { }
+  vehicleForm: FormGroup = new FormGroup({
+    cod_vehicle: new FormControl({value: '', disabled: true}),
+    typeTruck: new FormControl("",[Validators.required]),
+    registrationDate: new FormControl({value: new Date().toLocaleDateString('pt-BR'), disabled: true}),
+    plaque: new FormControl("",[Validators.required]),
+    color: new FormControl(""),
+    year: new FormControl("",[Validators.required]),
+    maximumWeight: new FormControl(""),
+    typeFuel: new FormControl("", [Validators.required]),
+    acquisitionKm: new FormControl("", [Validators.required]),
+    chassis: new FormControl("",[Validators.required]),
+    motor: new FormControl("",[Validators.required]),
+    reindeer: new FormControl("",[Validators.required]),
+    owner: new FormControl("",[Validators.required]),
+    vehicleDocument: new FormControl("",[Validators.required]),
+    vehiclePicture: new FormControl(""),
+    description: new FormControl(""),
+    rulesCheck: new FormControl(false,[Validators.required])
+  })
 
-  ngOnInit(): void {
+  registrationDataCurrent() {
+    
   }
-
 }
